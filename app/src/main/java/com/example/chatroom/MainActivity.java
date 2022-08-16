c29e0bc6ad51631e0983d632cad95afbd23ecb01
AesirNjord <850246624@qq.com>
2022-08-16 15:44:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chatroom/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.chatroom;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText account;
+    private EditText password;
+    private Button login;
+    private Button sign;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        account = findViewById(R.id.account);
+        password = findViewById(R.id.account);
+        login = findViewById(R.id.login);
+        sign = findViewById(R.id.sign);
+
+    }
+}
