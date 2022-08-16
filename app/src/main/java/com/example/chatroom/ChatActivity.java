df3660f5dee16e93d1fcf6ab61982e2f0edfc450
AesirNjord <850246624@qq.com>
2022-08-16 17:18:22 +0800

    1

---
+++ app/src/main/java/com/example/chatroom/ChatActivity.java
@@ -0,0 +1,7 @@
+package com.example.chatroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/chatroom/SignActivity.java
@@ -0,0 +1,6 @@
+package com.example.chatroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignActivity extends AppCompatActivity {
+}
